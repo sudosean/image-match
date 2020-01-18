a4736ba0b48efae59948ab5919b25cf4ce32e8ad
sean <sean.large1@gmail.com>
2020-01-18 17:26:52 -0500

    api pkg

--- cmd/myapp/main.go
+++ cmd/myapp/main.go
@@ -16,8 +16,48 @@ limitations under the License.
 
 package main
 
-import "log"
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/sudosean/image-match/pkg/api"
+)
 
 func main() {
-	log.Printf("hello, world!")
+	var (
+		httpAddr = flag.String("http", ":8080", "http listen address")
+	)
+	flag.Parse()
+	ctx := context.Background()
+	// our napodate service
+	srv := api.NewService()
+	errChan := make(chan error)
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT, syscall.SIGTERM)
+		errChan <- fmt.Errorf("%s", <-c)
+	}()
+
+	// mapping endpoints
+	endpoints := api.Endpoints{
+		GetEndpoint:      api.MakeGetEndpoint(srv),
+		StatusEndpoint:   api.MakeStatusEndpoint(srv),
+		ValidateEndpoint: api.MakeValidateEndpoint(srv),
+	}
+
+	// HTTP transport
+	go func() {
+		log.Println("api is listening on port:", *httpAddr)
+		handler := api.NewHTTPServer(ctx, endpoints)
+		errChan <- http.ListenAndServe(*httpAddr, handler)
+	}()
+
+	log.Fatalln(<-errChan)
 }
